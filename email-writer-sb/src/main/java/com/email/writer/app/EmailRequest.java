e8c0dd8468a872fdc82ea3ba3c48187b09a957b8
Rahul Kumar Jha <rahuljhakumar1600@gmail.com>
2025-03-21 23:35:21 +0530

    version 1.0.0

---
+++ email-writer-sb/src/main/java/com/email/writer/app/EmailRequest.java
@@ -0,0 +1,14 @@
+package com.email.writer.app;
+
+public class EmailRequest {
+String emailContent ;
+String tone;
+
+public String getTone() {
+    return tone ;
+}
+
+public String getEmailContent() {
+    return emailContent ;
+}
+}
